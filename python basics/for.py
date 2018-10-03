7d1385b10cf73dc457be587801f7cf8857195aac
mufc2310 <umarchauhan22@gmail.com>
2018-10-03 15:53:54 +0700

    basics2

---
+++ python basics/for.py
@@ -0,0 +1,17 @@
+# for i in range(11):
+#     print(i)
+
+#Wap to take start point and end point from user and print the range.
+#
+#
+# num1=int(input('Enter first num : '))
+# num2=int(input('Enter second num : '))
+#
+# for i in range (num1,num2):
+#     print(i)
+#
+
+num1=int(input('Enter first num : '))
+num2=int(input('Enter second num : '))
+num3=range(num1,num2)
+print(*num3)
