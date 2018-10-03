4ed73446fcf8cbbdd3c4bf0099a942fa434b90d4
mufc2310 <umarchauhan22@gmail.com>
2018-10-03 15:39:22 +0700

    basics2

---
+++ python basics/while.py
@@ -0,0 +1,35 @@
+# num=1
+# while(num<=5):
+#     print(num)
+#     num=num+1
+
+
+
+#Wap to check whether a given  number is divisble by 5.
+# num=int(input("Enter the number : "))
+#
+# if(num%5==0):
+#     print(num ,"is divisible by 5")
+#
+# else:
+#     print(num ,"is not divisible by 5")
+
+
+# Wap to print n natural numbers. ascending order
+# num=int(input('Enter the number : '))
+# i=1
+# while(i<=num):
+#     print(i)
+#     i+=1
+
+
+#Wap to find n natural numbers. descending
+# num=int(input('Enter the number : '))
+# i=0
+# while(i!=num):
+#     print(num)
+#     num-=1
+
+
+
+
