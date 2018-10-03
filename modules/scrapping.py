fbf8c772090dfc74d56b2edda9cb561ec96c37c8
mufc2310 <umarchauhan22@gmail.com>
2018-10-03 17:19:54 +0530

    scrapping2

--- modules/scrapping.py
+++ modules/scrapping.py
@@ -1,5 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 html_page = requests.get("https://www.imdb.com/chart/tvmeter?ref_=nv_tvv_mptv")
 
-print(html_page.text)
+# print(html_page.text)
+
+soup = BeautifulSoup(html_page.text,"lxml")
+for l in soup.find_all('h1'):    #find_all/find
+    print(l.text)
+# print(soup)
+
+a=soup.find("td",{'class':'titleColumn'})          #classname: tag
+a_tag=a.find('a')
+print(a_tag.text)
+
+b=soup.find("td",{'class':'lister-list'})
+b_tag=a.find('a')
+print(b_tag.text)
