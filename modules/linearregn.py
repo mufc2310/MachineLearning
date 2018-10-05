afcb89534388d4e9e2ae5954ece6f94bc55fa390
mufc2310 <umarchauhan22@gmail,=.com>
2018-10-05 16:25:36 +0530

    linear regression

---
+++ modules/linearregn.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("../Desktop/datasci-master/Final/Regression/Salary_Data.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+plt.plot(X, y)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+print('PREDICTED\t\tACTUAL')
+for i in range(len(y_pred)):
+    print(y_pred[i], '\t', y_train[i])
+
+plt.scatter(X_train, y_train, color='red', label='Some1')
+plt.plot(X_train, regressor.predict(X_train), color='blue', label='Some1')
+plt.title('Salary vs. Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.legend(loc='upper left', numpoints=1)
+plt.show()
+
+plt.scatter(X_test, y_test, color='cyan')
+plt.plot(X_train, regressor.predict(X_train), color='magenta')
+plt.title('Salary vs. Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
